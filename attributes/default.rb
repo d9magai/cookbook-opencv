b9799f33216bc72828d0a9d912c19482c8ba239c
d9magai <kumagai@sencorp.co.jp>
2015-06-27 19:02:00 +0900

    modify attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,7 @@
 #
 
 default["opencv"]["version"] = "2.4.11"
-default["opencv"]["url"] = "https://codeload.github.com/Itseez/opencv/zip/#{node['opencv']['version']}"
+
+default["opencv"]["url"]["2.4.11"] = "https://codeload.github.com/Itseez/opencv/zip/#{node['opencv']['version']}"
+default["opencv"]["checksum"]["2.4.11"] = "4a9e00fe1dd7888109c1bf06793d995ea805acd2c630c217db6cc5214949f81e"
 
